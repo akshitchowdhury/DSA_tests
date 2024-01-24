53b73dc2bd65ef1c5b4050d80124cb797fa22d55
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-24 18:54:54 +0530

    d3

---
+++ src/Two_sum.java
@@ -0,0 +1,12 @@
+public class Two_sum {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] store = new int[];
+
+        return store;
+    }
+}
