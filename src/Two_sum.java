6bfa4a5d24474c6bacbaf24e401a5d96d3e35212
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-24 19:09:48 +0530

    d4

--- src/Two_sum.java
+++ src/Two_sum.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Two_sum {
     public static void main(String[] args) {
 
@@ -5,7 +7,40 @@ public class Two_sum {
 
     public int[] twoSum(int[] nums, int target) {
 
-        int[] store = new int[];
+        int[] store = new int[2];
+
+         Arrays.sort(nums);
+
+         int first =0;
+         int last = nums.length-1;
+
+//         while(first<last){
+//
+//             if(nums[first]+nums[last]==target){
+//
+//                 store[0] = first;
+//                 store[1] = last;
+//             }
+//             else{
+//                 first++;
+//                 last--;
+//             }
+//
+//         }
+
+        for(int i=first; i< nums.length; i++){
+
+            for(int j=last; j>first; j--){
+                if(nums[first]+nums[last]==target){
+
+                    store[0] = first;
+                    store[1] = last;
+
+                }
+            }
+
+        }
+
 
         return store;
     }
